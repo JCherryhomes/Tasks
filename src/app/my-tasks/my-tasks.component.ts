eeecc89234fb59b53f51f088d5d6511511c5d6a7
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2017-01-20 23:41:41 -0600

    Added functionality for completing tasks

--- src/app/my-tasks/my-tasks.component.ts
+++ src/app/my-tasks/my-tasks.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
+import { ITask } from '../interfaces/ITask';
+import { MdButton } from '@angular/material';
 
 @Component({
   selector: 'app-my-tasks',
   templateUrl: './my-tasks.component.html',
   styleUrls: ['./my-tasks.component.css'],
-  providers: [TaskService]
+  providers: [TaskService, MdButton]
 })
 export class MyTasksComponent implements OnInit {
+  tasks: ITask[] = [
+    <ITask>{ complete: false, description: 'This is the first task' },
+    <ITask>{ complete: true, description: 'This is the second task' },
+    <ITask>{ complete: false, description: 'This is the third task' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setComplete(task: ITask): void {
+    task.complete = !task.complete;
+  }
+
+  addTask(): void {
+    // Testing
+  }
 }
