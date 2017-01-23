203e3fe297ac8eaf4d4047e9d2f738cd51b76866
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2017-01-23 06:03:12 -0600

    Added menu to toolbar

--- src/app/swim-lane/swim-lane.component.ts
+++ src/app/swim-lane/swim-lane.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MdGridList} from '@angular/material';
-import { DragulaService } from 'ng2-dragula';
+import {DragulaService} from 'ng2-dragula';
 import {MyTasksComponent} from '../my-tasks/my-tasks.component';
 import {SwimLane, Task, TaskState} from '../interfaces/Task';
-import { TaskService } from '../task.service';
+import {TaskService} from '../task.service';
 
 @Component({
   selector: 'app-swim-lane',
@@ -15,18 +15,25 @@ export class SwimLaneComponent implements OnInit {
   swimlanes: SwimLane[];
 
   constructor(private dragula: DragulaService, private service: TaskService) {
-    dragula.dropModel.subscribe((value) => {
-      console.log(value);
-      this.onDropModel(value.slice(1));
-    });
+    dragula
+      .dropModel
+      .subscribe((value) => {
+        console.log(value);
+        this.onDropModel(value.slice(1));
+      });
 
-    dragula.removeModel.subscribe((value) => {
-      this.onRemoveModel(value.slice(1));
-    });
+    dragula
+      .removeModel
+      .subscribe((value) => {
+        this.onRemoveModel(value.slice(1));
+      });
   }
 
   ngOnInit() {
-    this.service.getSwimlanes().subscribe(lanes => this.swimlanes = lanes);
+    this
+      .service
+      .getSwimlanes()
+      .subscribe(lanes => this.swimlanes = lanes);
   }
 
   private onDropModel(args): void {
